5a17a08a26be1ac32becf13d743f471159999124
Kabir Alberto <kabiralberto10@hotmail.com>
2019-09-30 17:56:36 -0500

    ee-tst

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
     </nav>
     <div class="container">
         <div class="jumbotron">
-            <h1>Learn to Create Websites</h1>
+            <h1>hello</h1>
             <p class="lead">In today's world internet is the most popular way of connecting with the people. At <a href="https://www.tutorialrepublic.com" target="_blank">tutorialrepublic.com</a> you will learn the essential web development technologies along with real life practice examples, so that you can create your own website to connect with the people around the world.</p>
             <p><a href="https://www.tutorialrepublic.com" target="_blank" class="btn btn-success btn-lg">Get started today</a></p>
         </div>
