7a02a2763936b5b2eeff6b2781950371129a14fe
valentin <praisethesun.u.filth@gmail.com>
2019-10-03 16:42:38 -0500

    creacion vistas parciales

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<hr>
+<div class="row">
+    
+    <div class="col-md-6 text-md-left">
+        <p></p>
+        <a href="#" class="text-dark"><strong>COMPAÑIA</strong></a><br><br> 
+        <a href="#" class="text-dark">Blog</a>
+        <span class="text-muted mx-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
+        <a href="#" class="text-dark">FAQ</a><br>
+        <a href="#" class="text-dark">Gif Cars</a>
+        <a href="#" class="text-dark">&nbsp;&nbsp;&nbsp;</a>
+        <a href="#" class="text-dark">Politica de Privacidad</a><br>
+        <a href="#" class="text-dark">Nosotros</a>
+        <a href="#" class="text-dark">&nbsp;</a>
+        <a href="#" class="text-dark">Términos y condiciones</a><br>
+        <a href="#" class="text-dark">Partners</a>
+        <a href="#" class="text-dark">&nbsp;&nbsp;&nbsp;</a>
+        <a href="#" class="text-dark">Become a Partener</a>
+    </div>
+    <div class="col-md-6 text-md-right">
+        <form>
+          <div class="form-group">
+            
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+            <small id="emailHelp" class="form-text text-muted">No compartirémos tu información con terceros.</small>
+          </div>
+          <button type="submit" class="btn btn-outline-info">Registrar</button>
+        </form>
+    </div>
+</div>
