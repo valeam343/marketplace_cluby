7c1c74f0acb9e5f4ba82b9fc729088ef33cbe77b
valeam343 <55755826+valeam343@users.noreply.github.com>
2019-10-07 10:06:00 -0500

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="navbar navbar-expand-md navbar-dark mb-3" style="background-color: #2e4da0; height: 56px;">
     <div class="container-fluid">
         <a href="#" class="navbar-brand mr-3"><h4><strong>Cluby</strong></h4></a>
@@ -8,16 +7,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav">
-<<<<<<< HEAD
                 <form >
-<<<<<<< HEAD
                     <div class="input-group mb-3" style="margin-top: 10px;">
-=======
-=======
-                <form>
->>>>>>> rama_valentin
-                    <div class="input-group mb-3">
->>>>>>> 7e1abb8785388d6fac86d7a4ad980425b5371d14
                         <div class="input-group-prepend">
                           <img src="{{asset('img/search.svg')}}">
                         </div>
@@ -40,51 +31,8 @@
                   </div>
                 <a href="#" class="nav-item nav-link">Guardado</a>
                 <a href="#" class="nav-item nav-link">Mensajes</a>
-<<<<<<< HEAD
                 <div style="border-left: 2px solid; color: white;"></div>
                 <span>&nbsp;&nbsp;</span><img src="{{asset('img/shop.svg')}}" width="5%">
-            
-=======
-                <div style="border-left: 2px solid;"></div>
-                <span>&nbsp;&nbsp;</span><img src="{{asset('img/cart.svg')}}">
->>>>>>> rama_valentin
-=======
-<nav class="navbar navbar-expand-md navbar-dark mb-3" style="background-color: #2e4da0">
-  <div class="container-fluid">
-    <a href="#" class="navbar-brand mr-3"><h4><strong>Cluby</strong></h4></a>
-
-    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <div class="navbar-nav">
-        <form>
-          <div class="input-group mb-3">
-            <div class="input-group-prepend">
-              <img src="{{asset('img/search.svg')}}">
-            </div>
-            <input type="text" class="form-control" placeholder="Username">
-          </div>
-        </form>
-      </div>  
-      <div class="navbar-nav ml-auto">
-        <div class="d-flex">
-          <div class="dropdown mr-1">
-            <button type="button" class="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20" style="color: white; font-size: 100%;">
-              Perfil
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
->>>>>>> rama_valentin
         </div>
-        <a href="#" class="nav-item nav-link">Guardado</a>
-        <a href="#" class="nav-item nav-link">Mensajes</a>
-        <div style="border-left: 2px solid; color: white;"></div>
-        <span>&nbsp;&nbsp;</span><img src="{{asset('img/cart.svg')}}">
-      </div>
     </div>    
-  </nav>
+</nav>
