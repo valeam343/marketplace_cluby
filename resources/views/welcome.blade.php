cc6b446b52cae44fd4e90a764f9e21a004429827
valeam343 <55755826+valeam343@users.noreply.github.com>
2019-10-22 18:10:54 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   @include('layouts.head')
-
   <link rel="stylesheet" type="text/css" href="{{asset('css/welcome.css')}}">
 </head>
 <body>
@@ -14,200 +13,191 @@
       <img src="img/ManchasHome/mancha (2).svg" id="mancha2">
       <img src="img/ManchasHome/mancha (3).svg" id="mancha3">
       <img src="img/ManchasHome/mancha (4).svg" id="mancha4">
-        <center><h1 class="lead">¿Listo para iniciar la aventura?</h1></center>
+      <center><h1 class="lead">¿Listo para iniciar la aventura?</h1></center>
 
-        <div class="row justify-content-md-center" style="margin-right: 0;
-    margin-left: 0;">
-          <div class="col-lg-8 col-md-8 col-xl-8" id="formSearch" style = "position: absolute;
-        top: 40%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 1;">
-            <form method="get" action="/filtro">
-              <div class="row" style="margin-right: 0; margin-left: 0;">
-                <div class="col-md-5">
-                  <input type="text" class="form-control" name="search" id="search" placeholder="Actividad" style="background-color: rgba(0, 0, 0, 0.1); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;
-                 ">
-               </div>
-               <div class="dropdown">
-                    <button type="button" class="btn btn-primary dropdown-toggle text-light btn-md" data-toggle="dropdown" style="background-color: rgba(0, 0, 0, 0.1); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;">Ciudad</button>
-                    <div class="dropdown-menu">
-                        @foreach($arr as $ac)
-                            <a class="dropdown-item" href="#">{{ucwords($ac['estadoProveedor'])}}</a>
-                        @endforeach
-                    </div>
-                </div>
-              <div class="col-md-2" >
-                <button type="submit" id="btnBuscar" class="btn btn-info btn-md">Empezar</button>
+      <div class="row justify-content-md-center" style="margin-right: 0;
+      margin-left: 0;">
+      <div class="col-lg-8 col-md-8 col-xl-8" id="formSearch" style = "position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 1;">
+      <form method="get" action="/filtro">
+        <div class="row" style="margin-right: 0; margin-left: 0;">
+          <div class="col-md-5">
+            <input autocomplete="false" type="text" class="form-control" name="search" id="search" placeholder="Actividad" style="background-color: rgba(0, 0, 0, 0.1); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;
+            ">
+          </div>
+          <!--<div class="dropdown" style="">
+            <button type="button" class="btn btn-primary dropdown-toggle text-light btn-md" data-toggle="dropdown" style="background-color: rgba(0, 0, 0, 0.1); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0; margin: 0 15% 5px;">Ciudad</button>
+            <div class="dropdown-menu">
+              @foreach($arr as $ac)
+              <a class="dropdown-item" href="#">{{ucwords($ac['estadoProveedor'])}}</a>
+              @endforeach
+            </div>
+          </div>-->
+          <div class="col-md-5">
+          <div class="form-group">
+            <div class="row">
+              <div class="col-md-2" style="padding-top: 10px;"><h6 style="color: white; font-weight: bold;">Ciudad</h6></div>
+              <div class="col-md-10">
+                <select class="form-control" id="exampleFormControlSelect1">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
               </div>
             </div>
-          </form>
+              
+              
+            </div>
+            </div>
+          <div class="col-md-2" >
+            <button type="submit" id="btnBuscar" class="btn btn-info btn-md">Empezar</button>
+          </div>
         </div>
-        <div style="display: block; float:right;  width:120px; height: 300px;">&nbsp;</div>
-      
+      </form>
     </div>
-    <center><img src="img/clubylogo.svg" style=" z-index: initial; width: 25%; opacity: .3; position: absolute; top: 45%;
-    left: 50%;
-    transform: translate(-50%, -45%);"></center>
+    <div style="display: block; float:right;  width:120px; height: 300px;">&nbsp;</div>
+
+  </div>
+  <center><img src="img/clubylogo.svg" style=" z-index: initial; width: 25%; opacity: .3; position: absolute; top: 45%;
+  left: 50%;
+  transform: translate(-50%, -45%);"></center>
 
 
-    <div class="row" style="margin-right: 0;
-    margin-left: 0;">
-      <div class="col-lg-12 col-md-12 col-xl-12 mb-2">
-        <!--<div class="container" id="categorias">-->
-          <div class="row" style="margin-right: 0; margin-left: 0;">
-            <div class="col-sm" id="slider">
-              <div class="slider-area slider">
-               <div class="slider variable-width myslider">
-                @foreach ($arrCategoria as $categoria)
-                <div>
-                  <a href="{{URL::to('categoria/'.$categoria['nomCategoria'])}}">
-                    <img class="img-fluid" src="{{asset($categoria['imagen'])}}" style="border-radius: 5px;">
-                    <center><h5 style="font-weight:bold; color: white; padding-top: 5px; ">{{ucwords($categoria['nomCategoria'])}}</h5></center>
-                  </a>
-                </div>
-                @endforeach
+  <div class="row" style="margin-right: 0;
+  margin-left: 0;">
+  <div class="col-lg-12 col-md-12 col-xl-12 mb-2">
+    <!--<div class="container" id="categorias">-->
+      <div class="row" style="margin-right: 0; margin-left: 0;">
+        <div class="col-sm" id="slider">
+          <div class="slider-area slider">
+           <div class="slider variable-width myslider">
+            @foreach ($arrCategoria as $categoria)
+            <div>
+              <a href="{{URL::to('categoria/'.$categoria['nomCategoria'])}}">
+                <img class="img-fluid" src="{{asset($categoria['imagen'])}}" style="border-radius: 5px;">
+                <center><h5 style="font-weight:bold; color: white; padding-top: 5px; ">{{$categoria['nomCategoria']}}</h5></center>
+              </a>
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <hr>
+      <div class="row">
+        <center><p style="text-align: justify; font-size: 22pt;">¡Hola! te damos la bienvenida a CLUBY, el Marketplace ideal para aquellos proveedores profesionales que desean ofrecer las mejores opciones académicas para la comunidad infantil local y para todos los padres de familia que buscan brindar a sus hijos clases extraescolares que complementan su desarrollo fisico y cognitivo de sus peques.</p></center>
+      </div>
+      <hr>
+      <center><h1>TOP ACTIVIDADES</h1></center>
+      <br>
+      <div class="container">
+
+        <div class="row">
+
+          @foreach ($arr as $cat)
+          <div class="col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch" style="margin-top: 10px;">
+            <div class="card">
+              <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}"><img style="height: 200px;" src="{{asset($cat['rutaimagen'])}}" class="responsive"></a>
+              <div class="card-body">
+                <h5 class="card-title">{{$cat['nomActividad']}}</h5>
+                <p class="card-text">{{$cat['desActividad']}}</p>
+              </div>
+              <div class="card-footer">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}" class="btn btn-outline-primary">Ver actividad</a>
               </div>
             </div>
           </div>
+          <div id="mybutton">
+            <a href="#nab"><img src="/img/up.png" id="fixedbutton" class="img-fluid" style="height: auto; width: 8%;"></a> 
+          </div>
+          @endforeach 
         </div>
-        <!--</div>-->
       </div>
     </div>
-  </div>
-</header>
 
-<div class="container">
-  <hr>
-  <div class="row">
-    <center><p style="text-align: justify; font-size: 22pt;">¡Hola! te damos la bienvenida a CLUBY, el Marketplace ideal para aquellos proveedores profesionales que desean ofrecer las mejores opciones académicas para la comunidad infantil local y para todos los padres de familia que buscan brindar a sus hijos clases extraescolares que complementan su desarrollo fisico y cognitivo de sus peques.</p></center>
-  </div>
-  <hr>
-  <center><h1>TOP ACTIVIDADES</h1></center>
-  <br>
-  <div class="container">
-    <div class="row">
-      @foreach ($arr as $cat)
-      <div class="col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch" style="margin-top: 10px;">
-        <div class="card">
-          <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}"><img style="height: 200px;" src="{{asset($cat['rutaimagen'])}}" class="responsive"></a>
-          <div class="card-body">
-            <h5 class="card-title">{{ucwords($cat['nomActividad'])}}</h5>
-            <p class="card-text">{{ucfirst($cat['desActividad'])}}</p>
-            
-          </div>
-          <div class="card-footer">
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}" class="btn btn-outline-primary">Ver actividad</a>
-          </div>
-        </div>
-
-      </div>
-      @endforeach
-    </div>
-  </div>
-  <div id="mybutton">
-    <a href="#nab"><img src="/img/up.png" id="fixedbutton" class="img-fluid" style="height: auto; width: 8%;"></a> 
-  </div>
-</div>
-@include('layouts.footer')
-<script type="text/javascript">
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-  });
-
-
-  $(document).ready(function() {
-   $( "#search" ).autocomplete({
-     source: function(request, response) {
-       $.ajax({
-         url: "{{url('searchs')}}",
-         data: {
-           term : request.term
-         },
-         dataType: "json",
-         success: function(data){
-          var resp = $.map(data,function(obj){
-           return obj.nomActividad || obj.nomCategoria;
-         }); 
-          response(resp);
+    <script type="text/javascript">
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
       });
-     },
-     minLength: 1
-   });
-
-   $( "#searchCiudad" ).autocomplete({
-     source: function(request, response) {
-       $.ajax({
-         url: "{{url('search')}}",
-         data: {
-           term : request.term
+      $(document).ready(function() {
+       $( "#search" ).autocomplete({
+         source: function(request, response) {
+           $.ajax({
+             url: "{{url('searchs')}}",
+             data: {
+               term : request.term
+             },
+             dataType: "json",
+             success: function(data){
+              var resp = $.map(data,function(obj){
+                return obj.nomActividad;
+             }); 
+              response(resp);
+            }
+          });
          },
-         dataType: "json",
-         success: function(data){
-          var resp = $.map(data,function(obj){
-           return obj.ciudad;
-         }); 
-          response(resp);
+         minLength: 1
+       });
+     })
+     </script>
+     <script type="text/javascript">
+      $('.myslider').slick({
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 770,
+          settings:{
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
+        ]
       });
-     },
-     minLength: 1
-   });
- });
-</script>
-
-<script type="text/javascript">
- $('.myslider').slick({
-   arrows: false,
-   dots: false,
-   infinite: true,
-   speed: 300,
-   slidesToShow: 5,
-   slidesToScroll: 5,
-   autoplay: true,
-   autoplaySpeed: 3000,
-   responsive: [
-   {
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      infinite: true,
-      dots: false
-    }
-  },
-  {
-    breakpoint: 770,
-    settings:{
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      infinite: true,
-    }
-  },
-  {
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 2,
-      slidesToScroll: 2
-    }
-  },
-  {
-    breakpoint: 480,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }
-  ]
-});
-</script>
-</body>
-</html>
+    </script>
+  </body>
+  @include('layouts.footer')
+  </html>
