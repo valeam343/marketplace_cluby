e18c7a2a5dc790cfd581c4fd50704d611685000c
valeam343 <55755826+valeam343@users.noreply.github.com>
2019-10-22 14:05:05 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,43 +6,6 @@
   <link rel="stylesheet" type="text/css" href="{{asset('css/welcome.css')}}">
 </head>
 <body>
-<<<<<<< HEAD
-    <header>
-        @include('layouts.header')
-        <div style="background-image: url('{{asset('vista/degardado bg.svg')}}');background-repeat: no-repeat, repeat; background-position: center; background-size: cover; position: relative;">
-            <img src="img/ManchasHome/mancha (1).svg" id="mancha1">
-            <img src="img/ManchasHome/mancha (2).svg" id="mancha2">
-            <img src="img/ManchasHome/mancha (3).svg" id="mancha3">
-            <img src="img/ManchasHome/mancha (4).svg" id="mancha4">
-            <center><h1 class="lead">¿Listo para iniciar la aventura?</h1></center>
-            <div class="row justify-content-md-center" >
-                <div class="col-lg-8 col-md-8 col-xl-8" id="formSearch" style = "position: absolute;
-                top: 40%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                padding: 3px 0 3px 0;
-                border-radius: 5px;
-                border: 2px solid white;
-                z-index: 1;">
-                <form method="get" action="/filtro">
-                    <div class="row">
-                        <div class="col-md-5">
-                            <input type="text" class="form-control" name="search" id="search" placeholder="Actividad" style="background-color: rgba(0, 0, 0, 0.1); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;">
-                        </div>
-                        <div class="dropdown">
-                            <button type="button" class="btn btn-primary dropdown-toggle text-light btn-sm" data-toggle="dropdown">Ciudad</button>
-                            <div class="dropdown-menu">
-                                @foreach($arrCategoria as $ac)
-                                    <a class="dropdown-item" href="#">{{ucwords($ac['ciudad'])}}</a>
-                                @endforeach
-                            </div>
-                        </div>
-                        <div class="col-md-2" >
-                            <button type="submit" id="btnBuscar" class="btn btn-info btn-md">Empezar</button>
-                        </div>
-                    </div>
-                </form>
-=======
 
   <header>
     @include('layouts.header')
@@ -72,30 +35,9 @@
               <div class="col-md-2" >
                 <button type="submit" id="btnBuscar" class="btn btn-info btn-md">Empezar</button>
               </div>
->>>>>>> c0606fb5725ccefad63efd9d486b9ca02dc88b83
             </div>
           </form>
         </div>
-<<<<<<< HEAD
-        <center><img src="img/clubylogo.svg" style=" z-index: initial; width: 25%; opacity: .3; position: absolute; top: 45%;left: 50%; transform: translate(-50%, -45%);"></center>
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-xl-12 mb-2">
-                <div class="row">
-                    <div class="col-sm" id="slider">
-                        <div class="slider-area slider">
-                            <div class="slider variable-width myslider">
-                                @foreach ($arrCategoria as $categoria)
-                                <div>
-                                    <a href="{{URL::to('categoria/'.$categoria['pkCategoria'])}}">
-                                        <img class="img-fluid" src="{{asset($categoria['imagen'])}}">
-                                        <center><h5 style="font-weight:bold; color: white; padding-top: 5px; ">{{ucwords($categoria['nomCategoria'])}}</h5></center>
-                                    </a>
-                                </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-=======
         <div style="display: block; float:right;  width:120px; height: 300px;">&nbsp;</div>
       
     </div>
@@ -118,7 +60,6 @@
                     <img class="img-fluid" src="{{asset($categoria['imagen'])}}" style="border-radius: 5px;">
                     <center><h5 style="font-weight:bold; color: white; padding-top: 5px; ">{{$categoria['nomCategoria']}}</h5></center>
                   </a>
->>>>>>> c0606fb5725ccefad63efd9d486b9ca02dc88b83
                 </div>
                 @endforeach
               </div>
@@ -132,8 +73,6 @@
 </header>
 
 <div class="container">
-<<<<<<< HEAD
-=======
   <hr>
   <div class="row">
     <center><p style="text-align: justify; font-size: 22pt;">¡Hola! te damos la bienvenida a CLUBY, el Marketplace ideal para aquellos proveedores profesionales que desean ofrecer las mejores opciones académicas para la comunidad infantil local y para todos los padres de familia que buscan brindar a sus hijos clases extraescolares que complementan su desarrollo fisico y cognitivo de sus peques.</p></center>
@@ -142,7 +81,6 @@
   <center><h1>TOP ACTIVIDADES</h1></center>
   <br>
   <div class="container">
->>>>>>> c0606fb5725ccefad63efd9d486b9ca02dc88b83
     <div class="row">
       @foreach ($arr as $cat)
       <div class="col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch" style="margin-top: 10px;">
