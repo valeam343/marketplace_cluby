69b0d74fd1829f3b9af0f113c85690383aeae231
valeam343 <55755826+valeam343@users.noreply.github.com>
2019-10-22 18:34:55 -0500

    Update actividadController.php

--- app/Http/Controllers/actividadController.php
+++ app/Http/Controllers/actividadController.php
@@ -8,23 +8,7 @@ class actividadController extends Controller
 {
     //
     public function show($id){
-<<<<<<< HEAD
-        try {
-                $client = new \GuzzleHttp\Client();
-                $request = $client->get('https://apicluby.azurewebsites.net/actividades/'.$id);
-                $requestGrupos = $client->get('https://apicluby.azurewebsites.net/gruposPorActividad/'.$id);
-                $response = $request->getBody();
-                $responseGrupos = $requestGrupos->getBody();
-                $contentGrupos = $responseGrupos->getContents();
-                $content = $response->getContents();
-                $act = json_decode($content, TRUE);
-                $grp = json_decode($contentGrupos, TRUE);
-                return view('actividad', compact('act', 'grp'));
-        } catch (Exception $e) {
-            echo "Exception: ".$e->getMessage();
-        }
-       }
-=======
+
 
     	try {
     			$client = new \GuzzleHttp\Client();
@@ -41,7 +25,6 @@ class actividadController extends Controller
     		echo "Exception: ".$e->getMessage();
     	}
     }
->>>>>>> b1ad1490577eadc103c98e2213ab95f7082ec78a
 
 
 }
