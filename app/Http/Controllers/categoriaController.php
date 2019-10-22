35692b1563cb2d4eeabf94d8b8f8984e0233f9a0
Kabir <kabiralberto@gmail.com>
2019-10-22 12:28:01 -0500

    22

--- app/Http/Controllers/categoriaController.php
+++ app/Http/Controllers/categoriaController.php
@@ -1,35 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use DB;
 use Redirect;
 class categoriaController extends Controller
 {
-    //
   public function index($arr, $con){
 
     return view('categoria')->with(['arrCategoria' => $arr])->with(['categoria' =>$con]);
   }
-    /*public function show($id){
-    	try {
-           $client = new \GuzzleHttp\Client();
-           $request = $client->get('https://apicluby.azurewebsites.net/actividades/'.$id);
-           $response = $request->getBody();
-           $content = $response->getContents();
-           $cat = json_decode($content, TRUE);
-           $requestAct = $client->get('http://192.168.1.73:8000/categoria/'.$id);
-           $responseAct = $requestAct->getBody();
-           $contentAct = $responseAct->getContents();
-           $act = json_decode($contentAct, TRUE);
-    			//return response()->json($actividad);
-           return view('categoria')->with(['cat' => $cat])->with(['act' => $act]);
-       } catch (Exception $e) {
-          echo "Exception: ".$e->getMessage();
-      }
-    }*/
-
     public function actividadesPorCategoria($id){
       try {
         $client = new \GuzzleHttp\Client();
@@ -47,8 +26,6 @@ class categoriaController extends Controller
 
     public function filtroEnInicio(Request $request){
       try {
-        //$actividad  = $input['search'];
-        //$ciudad     = $input['searchCiudad'];
         $actividad = $request->input('search');
         $ciudad = $request->input('searchCiudad');
         $client = new \GuzzleHttp\Client();
@@ -57,13 +34,10 @@ class categoriaController extends Controller
         }else{
           $request = $client->get('https://apicluby.azurewebsites.net/filtrar/second/'.$ciudad);
         }
-        
         $response = $request->getBody();
         $content = $response->getContents();
         $arrCategoria = json_decode($content, TRUE);
-
          return view('categoria')->with(['arrCategoria' => $arrCategoria])->with(['categoria' => ' ']);
-        
       } catch (Exception $e) {
         echo "Exception: ".$e->getMessage();
       }
