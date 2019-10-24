e85e8d51910e9018bceff27c2400d86bc4b19735
Your Name <you@example.com>
2019-10-24 11:43:49 -0500

    vista home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,51 +3,24 @@
 <head>
   @include('layouts.head')
   <link rel="stylesheet" type="text/css" href="{{asset('css/welcome.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('css/card.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('css/slider.css')}}">
   <style type="text/css">
-    .slick-slide img {
-    width: 100%;
-    max-width: 225px;
-    height: 205px;
-    object-fit: cover;
-    }
-    .espacio{
-    height: 112px;
-    }
-    @media(min-width: 1124px){
-      #primerPanel{
-        background-image: url('{{asset('vista/degardado bg.svg')}}');
-        background-repeat: no-repeat, repeat;
-        background-position: center;
-        background-size: cover;
-        position: relative;
-        height: 727px;
-      }
-    }
-    @media(max-width: 1123px){
-      #logo{
-        width: 38%!important;
-      }
-      #primerPanel{
-        background-image: url('{{asset('vista/degardado bg.svg')}}');
-        background-repeat: no-repeat, repeat;
-        background-position: center;
-        background-size: cover;
-        position: relative;
-        max-height: 727px!important;
-      }
-    }
-    @media(max-width: 900px){
-      #logo{
 
-        width: 50%!important;
-      }
-    }
-    @media(max-width: 650px){
-      #logo{
-        width: 70%!important;
-      }
-    }   
-  </style>
+.espacio{
+    height: 112px;
+}
+#primerPanel{
+    background-image: url('{{asset('vista/degardado bg.svg')}}');
+    background-repeat: no-repeat, repeat;
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    max-height: 727px!important;
+}
+
+ 
+</style>
 
 </head>
 <body>
@@ -67,42 +40,42 @@
       transform: translate(-50%, -50%);
       z-index: 1;">
       <form method="POST" action="/filtro">
-       {{ csrf_field() }}
-       <div class="row" style="margin-right: 0; margin-left: 0;">
+         {{ csrf_field() }}
+         <div class="row" style="margin-right: 0; margin-left: 0;">
 
-        <div class="col-md-5">
-          <div class="form-group">
-            <div class="row">
-              <div class="col-md-12">
-                <input autocomplete="false" type="text" class="form-control{{ $errors->has('search') ? ' has-error' : '' }}" name="search" id="search" placeholder="Actividad" style="background-color: rgba(0, 0, 0, 0.3); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;
-                ">
-                <small class="text-white">&nbsp;<strong>{{ $errors->first('search') }}</strong></small>
-              </div>
+            <div class="col-md-5">
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-md-12">
+                    <input autocomplete="false" type="text" class="form-control{{ $errors->has('search') ? ' has-error' : '' }}" name="search" id="search" placeholder="Actividad" style="background-color: rgba(0, 0, 0, 0.3); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;
+                    ">
+                    <small class="text-white">&nbsp;<strong>{{ $errors->first('search') }}</strong></small>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="col-md-5">
-          <div class="form-group{{ $errors->has('estado') ? ' has-error' : '' }}">
-            <div class="row">
-              <div class="col-md-12">
-                <select class="form-control" id="estado" name="estado" style="background-color: rgba(0, 0, 0, 0.3); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;">
-                  <option disabled selected value>Estado...</option>
-                  @foreach($arr as $cat)
-                  <option value="{{$cat['estadoProveedor']}}">{{$cat['estadoProveedor']}}</option>
-                  @endforeach()
-                </select>
-                <small class="text-white">&nbsp;<strong>{{ $errors->first('estado') }}</strong></small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-2" >
-          <button type="submit" id="btnBuscar" class="btn btn-info btn-md">Empezar</button>
-        </div>
+    </div>
+    <div class="col-md-5">
+      <div class="form-group{{ $errors->has('estado') ? ' has-error' : '' }}">
+        <div class="row">
+          <div class="col-md-12">
+            <select class="form-control" id="estado" name="estado" style="background-color: rgba(0, 0, 0, 0.3); color: white; border-left: 6px solid purple; border-top: 0; border-right: 0;">
+              <option disabled selected value>Estado...</option>
+              @foreach($arr as $cat)
+              <option value="{{$cat['estadoProveedor']}}">{{$cat['estadoProveedor']}}</option>
+              @endforeach()
+          </select>
+          <small class="text-white">&nbsp;<strong>{{ $errors->first('estado') }}</strong></small>
       </div>
-    </form>
   </div>
-  <div style="display: block; float:right;  width:120px; height: 300px;">&nbsp;</div>
+</div>
+</div>
+<div class="col-md-2" >
+  <button type="submit" id="btnBuscar" class="btn btn-info btn-md">Empezar</button>
+</div>
+</div>
+</form>
+</div>
+<div style="display: block; float:right;  width:120px; height: 300px;">&nbsp;</div>
 </div>
 <center><img id="logo" src="img/clubylogo.svg" style=" z-index: initial; width: 25%; opacity: .3; position: absolute; top: 45%;
 left: 50%;
@@ -114,91 +87,104 @@ margin-left: 0;">
     <div class="row">
       <div class="col-sm" id="slider">
         <div class="slider-area slider">
-         <div class="slider variable-width myslider">
-          @foreach ($arrCategoria as $categoria)
-          <div class="col">
-            <div class= "col-md-12">
-              <figure>
-            <a href="{{URL::to('categoria/'.$categoria['pkCategoria'])}}">
-              <img class="img-fluid" src="{{asset($categoria['imagen'])}}" style="border-radius: 5px;">
-            </a>
-            <figcaption style="font-weight:bold; color: white; padding-top: 5px;">{{$categoria['nomCategoria']}}</figcaption>
-            </figure>
-            </div>
+           <div class="slider variable-width myslider">
+              @foreach ($arrCategoria as $categoria)
+              <div class= "col-md-12">
+                <a href="{{URL::to('categoria/'.$categoria['pkCategoria'])}}">
+                  <img class="img-fluid" src="{{asset($categoria['imagen'])}}" style="border-radius: 5px; height: 200px;object-fit: cover;">
+              </a>
+              <center><p style="font-weight: bold;text-align: center;color: white;">{{$categoria['nomCategoria']}}</p></center>
           </div>
           @endforeach
-        </div>
       </div>
-    </div>
   </div>
+</div>
+</div>
 </div>
 </div>
 </div>
 </header>
 <div class="container">
-  <div class="espacio"></div> 
-  <div class="row">
-    <center><p style="text-align: justify; font-size: 22pt;">¡Hola! te damos la bienvenida a CLUBY, el Marketplace ideal para aquellos proveedores profesionales que desean ofrecer las mejores opciones académicas para la comunidad infantil local y para todos los padres de familia que buscan brindar a sus hijos clases extraescolares que complementan su desarrollo fisico y cognitivo de sus peques.</p></center>
-  </div>
-  <div class="espacio"></div>
-  <center><h1>TOP ACTIVIDADES</h1></center>
-  <br>
-  <div class="container">
+    <div class="container">
+      <div class="espacio"></div> 
+      <div class="container">
+
+      </div>
+      <div class="row">
+        <center><p style="text-align: justify; font-size: 18pt;">¡Hola! te damos la bienvenida a CLUBY, el Marketplace ideal para aquellos proveedores profesionales que desean ofrecer las mejores opciones académicas para la comunidad infantil local y para todos los padres de familia que buscan brindar a sus hijos clases extraescolares que complementan su desarrollo fisico y cognitivo de sus peques.</p></center>
+    </div>
+    <div class="espacio"></div>
+    <h1 style="font-size: 34pt;text-align: center;">TOP ACTIVIDADES</h1>
+    <br>
+</div>
+
+<div class="container">
 
     <div class="row">
 
       @foreach ($arr as $cat)
       <div class="col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch" style="margin-top: 10px;">
-        <div class="card">
-          <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}"><img style="height: 200px;" src="{{asset($cat['rutaimagen'])}}" class="responsive"></a>
-          <div class="card-body">
-            <h5 class="card-title">{{$cat['nomActividad']}}</h5>
-            <p class="card-text">{{$cat['desActividad']}}</p>
-          </div>
-          <div class="card-footer">
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}" class="btn btn-outline-primary">Ver actividad</a>
-          </div>
+        <div class="square-card">
+          <a href="{{URL::to('actividad/'.$cat['nomActividad'])}}" alt="square card">
+            <div class="card-header" style="background-image: url('{{asset($cat['rutaimagen'])}}');
+            background-repeat: no-repeat;
+            background-size: cover; background-color: #460053;">
+            <div class="card-header_overlay">
+                <span class="pill">$0.00</span>
+            </div>
         </div>
+        <div class="card-title">
+          <h4>{{$cat['nomActividad']}}</h4>   
+          <div style="margin-left: 20px;">
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+          </div>
       </div>
-      @endforeach 
-    </div>
-    <div id="mybutton">
-        <a href="#nab"><img src="/img/up.png" id="fixedbutton" class="img-fluid" style="height: auto; width: 8%;"></a> 
-      </div>
-  </div>
+      <!--
+      <div class="cta">
+       <a href="https://google.com">view recipe</a>
+        <a  href="">Ver</a>
+    </div>-->
+</a>
+</div>
+</div>
+@endforeach 
+</div>
+<div id="mybutton">
+    <a href="#nab"><img src="/img/up.png" id="fixedbutton" class="img-fluid" style="height: auto; width: 8%;"></a> 
+</div>
+</div>
 </div>
 
 <script type="text/javascript">
   $.ajaxSetup({
     headers: {
       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-  });
+  }
+});
 
   $(document).ready(function() {
-   $( "#search" ).autocomplete({
-     source: function(request, response) {
-       $.ajax({
-         url: "{{url('searchs')}}",
-         data: {
-           term : request.term
+     $( "#search" ).autocomplete({
+       source: function(request, response) {
+         $.ajax({
+           url: "{{url('searchs')}}",
+           data: {
+             term : request.term
          },
          dataType: "json",
          success: function(data){
           var resp = $.map(data,function(obj){
             return obj.nomActividad;
-          }); 
+        }); 
           response(resp);
-        }
-      });
+      }
+  });
      },
      minLength: 1
-   });
+ });
  })
 </script>
 <script type="text/javascript">
@@ -208,7 +194,7 @@ margin-left: 0;">
     infinite: true,
     speed: 260,
     slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
@@ -216,35 +202,35 @@ margin-left: 0;">
       breakpoint: 1024,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         infinite: true,
         dots: false
-      }
-    },
-    {
-      breakpoint: 770,
-      settings:{
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
     }
-    ]
-  });
+},
+{
+  breakpoint: 770,
+  settings:{
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+}
+},
+{
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 1
+}
+},
+{
+  breakpoint: 480,
+  settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+}
+}
+]
+});
 </script>
 </body>
 @include('layouts.footer')
